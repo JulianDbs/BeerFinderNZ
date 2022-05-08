26a99b229e725cf6b728ad101dba79065242bac2
JulianDbs <jdbsildesmode@gmail.com>
2022-05-08 20:24:29 -0300

    First Commit

---
+++ src/main/java/com/juliandbs/beerfindernz/BeerfindernzApplication.java
@@ -0,0 +1,13 @@
+package com.juliandbs.beerfindernz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerfindernzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerfindernzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juliandbs/beerfindernz/BeerfindernzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juliandbs.beerfindernz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerfindernzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
